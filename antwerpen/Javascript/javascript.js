343ffaba85c6bbb385ba8d9957685998588d4d7f
Selim Kaygun <s109672@ap.be>
2020-02-20 20:49:37 +0100

    jep

---
+++ antwerpen/Javascript/javascript.js
@@ -0,0 +1,14 @@
+let myMap = L.map('stationMap', {
+    center: [51.2301, 4.41774],
+    zoom: 16
+  });
+  
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+  }).addTo(myMap);
+  
+  let marker = L.marker([51.2301, 4.41774]).addTo(myMap);
+
+  
