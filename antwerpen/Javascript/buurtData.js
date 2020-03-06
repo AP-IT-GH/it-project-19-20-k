706f114f778bfcea7d229bf86994079853f3b0fe
youssefelhindaz <s111023@ap.be>
2020-03-06 20:19:28 +0100

    Volledige naam toegevoegd in footer, express en ejs toegevoegd in buurtData.js

---
+++ antwerpen/Javascript/buurtData.js
@@ -0,0 +1,23 @@
+let fetch = require('node-fetch');
+const express = require('express');
+const ejs = require('ejs');
+
+fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek8/MapServer/850/query?where=1%3D1&outFields=OBJECTID,NAAM,TYPE,NIVEAU,OMSCHRIJVING,POSTCODE,DISTRICT,STAD&outSR=4326&f=json')
+.then(res => res.json())
+.then(buurtData => {
+    console.log(buurtData);
+});
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+//app.get('/', (req,res) => {
+//
+//});
+
+app.listen(app.get('port'), () => {
+  console.log(`Express started on http://localhost:${
+    app.get('port')}; press Ctrl-C to terminate.`);
+});
