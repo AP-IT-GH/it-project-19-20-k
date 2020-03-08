343d6dad9a41c6cfa27285177629a3f72ee0bd43
youssefelhindaz <s111023@ap.be>
2020-03-08 15:31:09 +0100

    Alles bestanden/mappen in route gezet

--- API/buurtData.js
+++
@@ -1,8 +0,0 @@
-let fetch = require('node-fetch');
-
-fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek8/MapServer/850/query?where=1%3D1&outFields=OBJECTID,NAAM,TYPE,NIVEAU,OMSCHRIJVING,POSTCODE,DISTRICT,STAD&outSR=4326&f=json')
-.then(res => res.json())
-.then(buurtData => {
-    console.log(buurtData);
-});
-
