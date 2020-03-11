357e255f7b46d01f2db1f1223cf1744ce153d99e
Burak Ocbe <s116768@ap.be>
2020-03-11 19:06:18 +0100

    Filteren en locatie opvragen

--- antwerpen/public/javascript/leaflet.js
+++ antwerpen/public/javascript/leaflet.js
@@ -30,3 +30,58 @@ let myMap = L.map('stationMap', {
     console.log(arrayStadsdeel[0].attributes.NAAM);
   });
 
+//Locatie opvragen
+myMap.locate({setView: true, maxZoom: 13});
+
+function onLocationFound(e) {
+let radius = e.accuracy;
+
+L.marker(e.latlng).addTo(myMap)
+    .bindPopup("Je bent " + radius + " meter in de buurt").openPopup();
+
+L.circle(e.latlng, radius).addTo(myMap);
+}
+
+myMap.on('locationfound', onLocationFound);
+//
+
+
+// Layers
+let cities = L.layerGroup();
+
+L.marker([51.313650414747784, 4.40437144959813]).bindPopup('This is Littleton, CO.').addTo(cities),
+L.marker([51.313233389604555, 4.404821323883451]).bindPopup('This is Denver, CO.').addTo(cities),
+L.marker([51.313189079057175, 4.404715153363371]).bindPopup('This is Aurora, CO.').addTo(cities),
+L.marker([51.31313922049221, 4.404622244716165]).bindPopup('This is Golden, CO.').addTo(cities);
+
+
+let anvers = L.layerGroup();
+
+L.marker([51.2211, -4.4021]).bindPopup('This is Littleton, CO.').addTo(anvers);
+
+let mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+let grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+    streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
+
+let map = L.map('map', {
+    center: [39.73, -104.99],
+    zoom: 10,
+    layers: [grayscale, cities,anvers]
+});
+
+let baseLayers = {
+    "Grayscale": grayscale,
+    "Streets": streets
+};
+
+let overlays = {
+    "Buurt gebruiksgroen": cities,
+    "Stadsdeel gebruiksgroen": anvers
+};
+
+L.control.layers(baseLayers, overlays).addTo(myMap);
+//
