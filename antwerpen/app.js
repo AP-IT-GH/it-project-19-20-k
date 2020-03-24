661d43f6f96d2183035595ba0af44368e256025d
youssefelhindaz <s111023@ap.be>
2020-03-25 00:54:54 +0100

    Titels veranderd, routing toegevoegd (niet werkend)

--- antwerpen/app.js
+++ antwerpen/app.js
@@ -1,15 +1,6 @@
 let fetch = require('node-fetch');
 const express = require('express');
 const ejs = require('ejs');
-
-
-//API buurt-gebruiksgroen
-/*fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek8/MapServer/850/query?where=1%3D1&outFields=OBJECTID,NAAM,TYPE,NIVEAU,OMSCHRIJVING,POSTCODE,DISTRICT,STAD&outSR=4326&f=json')
-.then(res => res.json())
-.then(buurtData => {
-    console.log(buurtData);
-});*/
-
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
