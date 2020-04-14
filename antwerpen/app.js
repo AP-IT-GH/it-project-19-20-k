926a3d7e595f64102e8b7ea5f1fd73a9e783eaa7
Burak Ocbe <s116768@ap.be>
2020-04-14 13:46:10 +0200

    Routing

--- antwerpen/app.js
+++ antwerpen/app.js
@@ -2,14 +2,6 @@ let fetch = require('node-fetch');
 const express = require('express');
 const ejs = require('ejs');
 
-
-//API buurt-gebruiksgroen
-/*fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek8/MapServer/850/query?where=1%3D1&outFields=OBJECTID,NAAM,TYPE,NIVEAU,OMSCHRIJVING,POSTCODE,DISTRICT,STAD&outSR=4326&f=json')
-.then(res => res.json())
-.then(buurtData => {
-    console.log(buurtData);
-});*/
-
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
@@ -21,18 +13,13 @@ app.get('/', (req,res) => {
 
 app.get('/stadsdeel', (req,res) =>{
   //API stadsdeel-gebruiksgroen
-<<<<<<< HEAD
 fetch('https://opendata.arcgis.com/datasets/593e9680b43e4332952d3ef249e1912a_854.geojson')
-=======
-fetch('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek8/MapServer/854/query?where=1%3D1&outFields=*&outSR=4326&f=json')
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
 .then(res => res.json())
 .then(stadsdeelData => {
     res.json(stadsdeelData);
 });
 });
 
-<<<<<<< HEAD
 app.get('/buurt', (req,res) =>{
   //API buurt-gebruiksgroen
 fetch('https://opendata.arcgis.com/datasets/c96c56b2c36f48cc86fbe77ea872b555_850.geojson')
@@ -42,8 +29,6 @@ fetch('https://opendata.arcgis.com/datasets/c96c56b2c36f48cc86fbe77ea872b555_850
 });
 });
 
-=======
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
 app.listen(app.get('port'), () => {
   console.log(`Express started on http://localhost:${
     app.get('port')}; press Ctrl-C to terminate.`);
--- antwerpen/public/javascript/leaflet.js
+++ antwerpen/public/javascript/leaflet.js
@@ -12,27 +12,7 @@ let myMap = L.map('stationMap', {
   }).addTo(myMap);
   
 
-<<<<<<< HEAD
 
-=======
-  fetch('/stadsdeel')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.features);
-    L.geoJSON(data).addTo(myMap);
-    let arrayStadsdeel = [];
-    for(let i = 0;i < data.features.length; i++){
-      arrayStadsdeel[i] =  data.features[i];
-    }
-    
-    console.log(arrayStadsdeel.attributes.NAAM)
-
-
-    console.log(arrayStadsdeel[0].attributes.NAAM);
-  });
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
 
 //Locatie opvragen
 myMap.locate({setView: true, maxZoom: 13});
@@ -47,13 +27,17 @@ L.circle(e.latlng, radius).addTo(myMap);
 }
 
 myMap.on('locationfound', onLocationFound);
-<<<<<<< HEAD
 
 //Kleur voor polygons
 let myStyleStadsdeel = {
   fillColor: 'darkgreen',
   color: 'green'
 };
+
+let myStyleBuurt ={
+  fillColor: 'darkpurple',
+  color: 'purple'
+}
 
 // Layers
 let stadsdeel = L.layerGroup();
@@ -79,33 +63,27 @@ fetch('/stadsdeel')
 })
 
 //API buurt-gebruiksgroen 
-/*fetch('/buurt')
+fetch('/buurt')
 .then((response) => {
   return response.json();
 })
 .then(data => {
-});*/
+  console.log(data.features);
+  L.geoJSON(data, {
+  onEachFeature: (features, layer) => {
+      let naam = features.properties.NAAM;
+      let postcode = features.properties.POSTCODE;
+      let district = features.properties.DISTRICT;
+      let omschrijving = features.properties.OMSCHRIJVING;
+      layer.setStyle(myStyleBuurt);
+      layer.bindPopup(`<div class = 'popup'>${naam}</div> <br> <div class = 'popup'>${postcode}</div> 
+      <div class = 'popup'>${district}</div> <br> <div class = 'popup'>${omschrijving}</div>`);
+    }
+  }).addTo(buurt);
+})
 
-let buurt = L.layerGroup();
 
 L.marker([51.2211, -4.4021]).bindPopup('This is Littleton, CO.').addTo(buurt);
-=======
-//
-
-
-// Layers
-let cities = L.layerGroup();
-
-L.marker([51.313650414747784, 4.40437144959813]).bindPopup('This is Littleton, CO.').addTo(cities),
-L.marker([51.313233389604555, 4.404821323883451]).bindPopup('This is Denver, CO.').addTo(cities),
-L.marker([51.313189079057175, 4.404715153363371]).bindPopup('This is Aurora, CO.').addTo(cities),
-L.marker([51.31313922049221, 4.404622244716165]).bindPopup('This is Golden, CO.').addTo(cities);
-
-
-let anvers = L.layerGroup();
-
-L.marker([51.2211, -4.4021]).bindPopup('This is Littleton, CO.').addTo(anvers);
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
 
 let mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -118,11 +96,7 @@ let grayscale   = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoom
 let map = L.map('map', {
     center: [39.73, -104.99],
     zoom: 10,
-<<<<<<< HEAD
     layers: [grayscale, stadsdeel,buurt]
-=======
-    layers: [grayscale, cities,anvers]
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
 });
 
 let baseLayers = {
@@ -131,19 +105,21 @@ let baseLayers = {
 };
 
 let overlays = {
-<<<<<<< HEAD
     "Buurt gebruiksgroen": buurt,
     "Stadsdeel gebruiksgroen": stadsdeel
 };
 
 L.control.layers(baseLayers, overlays).addTo(myMap);
 //
+//routing werkt nog niet
 
-=======
-    "Buurt gebruiksgroen": cities,
-    "Stadsdeel gebruiksgroen": anvers
-};
+let control = L.Routing.control({
+  waypoints: [
+      L.latLng(57.74, 11.94),
+      L.latLng(57.6792, 11.949)
+  ],
+  routeWhileDragging: true
+});
 
-L.control.layers(baseLayers, overlays).addTo(myMap);
-//
->>>>>>> 357e255f7b46d01f2db1f1223cf1744ce153d99e
+L.Routing.errorControl(control).addTo(myMap);
+
