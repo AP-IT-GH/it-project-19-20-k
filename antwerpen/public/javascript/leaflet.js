19777e9f93002194275541a7c77fe83aade3fe11
Burak Ocbe <s116768@ap.be>
2020-04-22 18:55:30 +0200

    Routing

--- antwerpen/public/javascript/leaflet.js
+++ antwerpen/public/javascript/leaflet.js
@@ -5,28 +5,12 @@ let myMap = L.map('stationMap', {
     zoom: 12
   });
   
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.i/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-  }).addTo(myMap);
-  
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.i/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+}).addTo(myMap);
 
-
-
-//Locatie opvragen
-myMap.locate({setView: true, maxZoom: 13});
-
-function onLocationFound(e) {
-let radius = e.accuracy;
-
-L.marker(e.latlng).addTo(myMap)
-    .bindPopup("Je bent " + radius + " meter in de buurt").openPopup();
-
-L.circle(e.latlng, radius).addTo(myMap);
-}
-
-myMap.on('locationfound', onLocationFound);
 
 //Kleur voor polygons
 let myStyleStadsdeel = {
@@ -44,6 +28,7 @@ let stadsdeel = L.layerGroup();
 let buurt = L.layerGroup();
 
 //API stadsdeel-gebruiksgroen
+let stadArray = [];
 fetch('/stadsdeel')
 .then((response) => {
   return response.json();
@@ -55,6 +40,9 @@ fetch('/stadsdeel')
       let postcode = features.properties.POSTCODE;
       let district = features.properties.DISTRICT;
       let omschrijving = features.properties.OMSCHRIJVING;
+      for (let i = 0; i < data.features.length; i++) {
+        stadArray[i] = data.features[i];
+      }
       layer.setStyle(myStyleStadsdeel);
       layer.bindPopup(`<div class = 'popup'>${naam}</div> <br> <div class = 'popup'>${postcode}</div> 
       <div class = 'popup'>${district}</div> <br> <div class = 'popup'>${omschrijving}</div>`);
@@ -63,30 +51,29 @@ fetch('/stadsdeel')
 })
 
 //API buurt-gebruiksgroen 
-let array = [];
+let buurtArray = [];
 fetch('/buurt')
 .then((response) => {
   return response.json();
 })
 .then(data => {
-  console.log(data.features);
   L.geoJSON(data, {
   onEachFeature: (features, layer) => {
       let naam = features.properties.NAAM;
       let postcode = features.properties.POSTCODE;
       let district = features.properties.DISTRICT;
       let omschrijving = features.properties.OMSCHRIJVING;
-      const list = document.getElementById("lijst");
-        for (let i = 0; i < data.features.length; i++) {
-          array[i] = data.features[i];
-          let infoNaam = document.createElement("li");
-          let infoDistrictPostcode = document.createElement("li");
-          let listOmschrijving = document.createElement("li");
-          infoNaam.textContent = "Naam: " +  data.features[i].properties.NAAM;
-          infoDistrictPostcode = "Gemeente: " + `${data.features[i].properties.DISTRICT} ${data.features[i].properties.POSTCODE}`;
-          listOmschrijving.textContent = "Omschrijving: " + data.features[i].properties.OMSCHRIJVING;
-          list.append(infoNaam, infoDistrictPostcode, listOmschrijving);
-        }
+      //const list = document.getElementById("lijst");
+      for (let i = 0; i < data.features.length; i++) {
+        buurtArray[i] = data.features[i];
+        /*let infoNaam = document.createElement("li");
+        let infoDistrictPostcode = document.createElement("li");
+        let listOmschrijving = document.createElement("li");
+        infoNaam.textContent = "Naam: " +  data.features[i].properties.NAAM;
+        infoDistrictPostcode = "Gemeente: " + `${data.features[i].properties.DISTRICT} ${data.features[i].properties.POSTCODE}`;
+        listOmschrijving.textContent = "Omschrijving: " + data.features[i].properties.OMSCHRIJVING;
+        list.append(infoNaam, infoDistrictPostcode, listOmschrijving);*/
+      }
       layer.setStyle(myStyleBuurt);
       layer.bindPopup(`<div class = 'popup'>${naam}</div> <br> <div class = 'popup'>${postcode}</div> 
       <div class = 'popup'>${district}</div> <br> <div class = 'popup'>${omschrijving}</div>`);
@@ -94,8 +81,8 @@ fetch('/buurt')
   }).addTo(buurt);
 })
 
-
-L.marker([51.2211, -4.4021]).bindPopup('This is Littleton, CO.').addTo(buurt);
+console.log(stadArray);
+console.log(buurtArray);
 
 let mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -123,26 +110,52 @@ let overlays = {
 
 L.control.layers(baseLayers, overlays).addTo(myMap);
 //
-//routing werkt nog niet
 
-let control = L.Routing.control(L.extend(window.lrmConfig, {
-	waypoints: [
-		L.latLng(57.74, 11.94),
-		L.latLng(57.6792, 11.949)
-	],
-	geocoder: L.Control.Geocoder.nominatim(),
-	routeWhileDragging: true,
-	reverseWaypoints: true,
-	showAlternatives: true,
-	altLineOptions: {
-		styles: [
-			{color: 'black', opacity: 0.15, weight: 9},
-			{color: 'white', opacity: 0.8, weight: 6},
-			{color: 'blue', opacity: 0.5, weight: 2}
-		]
-	}
-})).addTo(myMap);
+//Locatie opvragen
+myMap.locate({setView: true, maxZoom: 13});
+function onLocationFound(e) {
+let radius = e.accuracy;
+//L.marker(e.latlng).addTo(myMap)
+    //.bindPopup("Je bent " + radius + " meter in de buurt").openPopup();
+//L.circle(e.latlng, radius).addTo(myMap);
+}
 
-L.Routing.errorControl(control).addTo(myMap);
+//routing
+let control = L.Routing.control({
+  waypoints: [
+    L.latLng(myMap.on('locationfound', onLocationFound)),
+  ],
+  routeWhileDragging: true,
+  collapsible: true,
+  router: L.Routing.mapbox('pk.eyJ1IjoiYWJkdXJyYWhtYW5kdW5kYXIiLCJhIjoiY2s5N3U3eHV0MDBveTNucWl1OTNtZDc3cyJ9.ZvvJHZHkuqIst2eBTRPzfA')
+}).addTo(myMap);
 
+//Voor routing click route
+function createButton(label, container) {
+  let btn = L.DomUtil.create('button', '', container);
+  btn.setAttribute('type', 'button');
+  btn.innerHTML = label;
+  return btn;
+}
+
+myMap.on('click', function(e) {
+  let container = L.DomUtil.create('div'),
+      startBtn = createButton('Start van deze locatie', container),
+      destBtn = createButton('Ga naar deze locatie', container);
+
+  L.popup()
+      .setContent(container)
+      .setLatLng(e.latlng)
+      .openOn(myMap);
+      
+  L.DomEvent.on(startBtn, 'click', function() {
+    control.spliceWaypoints(0, 1, e.latlng);
+    myMap.closePopup();
+  });
+  
+  L.DomEvent.on(destBtn, 'click', function() {
+    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+    myMap.closePopup();
+  });
+});
 
