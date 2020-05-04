90ad430e565682c51dbf8e89575cb57227cd7529
youssefelhindaz <s111023@ap.be>
2020-05-04 23:43:46 +0200

    Filter en switch in orde

--- antwerpen/public/javascript/lijstAPI.js
+++ antwerpen/public/javascript/lijstAPI.js
@@ -3,10 +3,12 @@ let stadsdeelFetch = fetch('/stadsdeel')
 .then(stadsdeelData => {
     let stadFeatures = stadsdeelData.features;
     let stadArr = [];
+    let stadArrDistinct = [];
     for(let i = 0; i < stadFeatures.length; i++){
         stadArr.push(stadFeatures[i].properties.NAAM);
     }
-    showList(stadArr, 'lijstStadsdeel');
+    stadArrDistinct = stadArr.filter((n, i) => stadArr.indexOf(n) === i);
+    showList(stadArrDistinct, 'lijstStadsdeel');
 });
 
 //API buurt-gebruiksgroen
@@ -15,10 +17,12 @@ let buurtFetch = fetch('/buurt')
 .then(buurtData => {
     let buurtFeatures = buurtData.features;
     let buurtArr = [];
+    let buurtArrDistinct = [];
     for(let i = 0; i < buurtFeatures.length; i++){
         buurtArr.push(buurtFeatures[i].properties.NAAM);
     }
-    showList(buurtArr, 'lijstBuurt');
+    buurtArrDistinct = buurtArr.filter((n, i) => buurtArr.indexOf(n) === i);
+    showList(buurtArrDistinct, 'lijstBuurt');
 });
 
 
