4a23bc0ea5ad10b62bb0aebc77bc59760aec8f05
youssefelhindaz <s111023@ap.be>
2020-05-04 23:17:10 +0200

    Lijst achter een switch gezet. Code geoptimaliseerd.

--- antwerpen/public/javascript/lijstAPI.js
+++ antwerpen/public/javascript/lijstAPI.js
@@ -6,11 +6,8 @@ let stadsdeelFetch = fetch('/stadsdeel')
     for(let i = 0; i < stadFeatures.length; i++){
         stadArr.push(stadFeatures[i].properties.NAAM);
     }
-    showList(stadArr);
+    showList(stadArr, 'lijstStadsdeel');
 });
-
-
-
 
 //API buurt-gebruiksgroen
 let buurtFetch = fetch('/buurt')
@@ -21,15 +18,24 @@ let buurtFetch = fetch('/buurt')
     for(let i = 0; i < buurtFeatures.length; i++){
         buurtArr.push(buurtFeatures[i].properties.NAAM);
     }
-    showList(buurtArr);
+    showList(buurtArr, 'lijstBuurt');
 });
 
 
-function showList(giveArray) {
-    let list = document.getElementById("lijstAPI"); 
+function showList(giveArray, id) {
+    let list = document.getElementById(id); 
     giveArray.forEach(stadsdeel => {
-        let listElement = document.createElement("li");
+        let listElement = document.createElement('li');
         listElement.textContent = stadsdeel;
         list.appendChild(listElement);
     });
 }
+
+function showDiv() {
+    let x = document.getElementById('lijstAPI');
+    if(x.style.display === 'block'){
+        x.style.display = 'none';
+    } else{
+        x.style.display = 'block';
+    }
+ }
